560b48725cfb0461d5c16f2da43f21b52b348889
jimin040531 <jiminjimin0531@naver.com>
2024-11-13 18:14:03 +0900

    [Add] Main

---
+++ src/main/java/deu/hms/javaproject/JavaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.hms.javaproject;
+
+/**
+ *
+ * @author Jimin
+ */
+public class JavaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
